7365d363493d93f891162f9062692b85f424d3d6
Zarkozy <personal@zarkozy.com>
2026-04-22 17:14:50 -0400

    ungenericizing

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const countdown = { date: new Date("Jan 1, 2030 00:00:00").getTime(), title: "CHANGE ME VIA INDEX.JS" }
+const countdown = { date: new Date("May 5, 2026 00:00:00").getTime(), title: "SEMESTER END" }
 const element = document.getElementById("countdown");
 const title = document.getElementById("title");
 
@@ -6,8 +6,9 @@ function calculateTime(distance) {
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    return { days, hours, minutes, seconds };
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);    
+    const millis = distance % 1000;
+    return { days, hours, minutes, seconds, millis };
 }
 
 function updateClock() {
@@ -16,11 +17,15 @@ function updateClock() {
     if (distance < 0) {
         element.innerHTML = "It's Over.";
     } else {
-        const { days, hours, minutes, seconds } = calculateTime(distance);
-        element.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+        const { days, hours, minutes, seconds, millis } = calculateTime(distance);
+        element.innerHTML = `${pad(days, 2)}:${pad(hours, 2)}:${pad(minutes, 2)}:${pad(seconds, 2)}:${pad(millis, 3)}`;
     }
+}
+
+function pad(num, size) {
+  return String(num).padStart(size, '0');
 }
 
 title.innerHTML = countdown.title;
 updateClock();
-setInterval(updateClock, 1000);
+setInterval(updateClock, 1);
